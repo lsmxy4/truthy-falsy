9422c48573fc3d92060de61a51010212a3b802fc
unknown <lsmxy4@gmail.com>
2026-02-03 09:20:04 +0900

    fixed name

---
+++ 5.구조분해 할당/5-1.js
@@ -0,0 +1,21 @@
+const fruits = ["Apple", "Banana", "Orange"];
+
+const[first,second,third] = fruits
+
+console.log(first)
+console.log(second)
+console.log(third)
+
+const numbers = [1, 2, 3, 4, 5];
+
+const[num1, ,num2] = numbers
+
+console.log(num1)
+console.log(num2)
+
+
+const colors = ['red', 'greeen']
+
+const[color1,color2,color3] = colors
+
+console.log(color1,color2,color3)
